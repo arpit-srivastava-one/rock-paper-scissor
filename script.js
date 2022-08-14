b351a7d759a458eec6bc1ef458d6e98a5592faae
Arpit Srivastava <srivastavaarpit121@gmail.com>
2022-08-14 17:18:46 +0530

    Create function to get computer choice

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ function getPlayerChoice() {
     action = action.toLocaleLowerCase();
     return action;
 }
+
+
+function getComputerChoice() {
+    let rnd = getRandomInteger(1, 4);
+    let choice = "";
+
+    switch (rnd) {
+        case 1:
+            choice = "rock";
+            break;
+        case 2:
+            choice = "paper";
+            break;
+        case 3:
+            choice = "scissor";
+            break;
+        default:
+            choice = "invalid";
+    }
+
+    return choice;
+}
+
+
+// The following page helped me get the solution - https://www.w3schools.com/js/js_random.asp
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
