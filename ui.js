d1b40c51858fc7d21151bd886e05cc5b9fbbcf20
Arpit Srivastava <srivastavaarpit121@gmail.com>
2022-09-07 23:23:29 +0530

    Disable interaction of other cards, when one is selected

--- ui.js
+++ ui.js
@@ -1,7 +1,28 @@
 const cards = document.querySelectorAll(".card");
+
 cards.forEach(card => {
     card.addEventListener("click", e => {
         card.classList.remove("card");
         card.classList.add("card-selected");
+
+        turnOffInteractioncards();
     });
+});
+
+const turnOnInteractioncards = () => {
+    cards.forEach(card => {
+        card.style.pointerEvents = "auto";
+    });
+};
+
+const turnOffInteractioncards = () => {
+    cards.forEach(card => {
+        card.style.pointerEvents = "none";
+    });
+};
+
+// For debugging
+window.addEventListener("keydown", e => {
+    if (e.key === "R" || e.key === "r")
+        turnOnInteractioncards();
 });
