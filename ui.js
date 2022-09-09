8c379d455e32801081c1a7983ad70772f1273c17
Arpit Srivastava <srivastavaarpit121@gmail.com>
2022-09-09 22:50:31 +0530

    Add intro, by making footer interactable

--- ui.js
+++ ui.js
@@ -1,28 +1,44 @@
+// ---------------------------------Initialize---------------------------------
 const playerCards = document.querySelectorAll(".middle .left-section .card");
+const footer = document.querySelector(".footer");
+const footerText = document.querySelector(".footer h2");
 
+
+// ---------------------------------Start---------------------------------
+footerText.textContent = "The first to score 5 points... wins the game!";
+turnOffInteractionCards();
+
+// ---------------------------------Cards---------------------------------
 playerCards.forEach(card => {
     card.addEventListener("click", e => {
         card.classList.remove("card");
         card.classList.add("card-selected");
 
-        turnOffInteractioncards();
+        turnOffInteractionCards();
     });
 });
 
-const turnOnInteractioncards = () => {
+function turnOnInteractionCards() {
     playerCards.forEach(card => {
         card.style.pointerEvents = "auto";
     });
 };
 
-const turnOffInteractioncards = () => {
+function turnOffInteractionCards() {
     playerCards.forEach(card => {
         card.style.pointerEvents = "none";
     });
 };
 
-// For debugging
+// Cards - For debugging
 window.addEventListener("keydown", e => {
     if (e.key === "R" || e.key === "r")
-        turnOnInteractioncards();
+        turnOnInteractionCards();
+});
+
+
+// ---------------------------------Footer---------------------------------
+footer.addEventListener("click", () => {
+    footerText.textContent = "Your turn.";
+    turnOnInteractionCards();
 });
