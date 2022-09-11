caedc17e88a347a1b3150f819884b8823ceed201
Arpit Srivastava <srivastavaarpit121@gmail.com>
2022-09-11 16:22:30 +0530

    Refactor project files

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice);
+// ---------------------------------Initialize---------------------------------
+const playerCards = document.querySelectorAll(".middle .left-section .card");
 
-        console.log(`Round ${i+1}: ${result}`);
-    }
+const footer = document.querySelector(".footer");
+const footerText = document.querySelector(".footer h2");
+
+const playerScoreDiv = document.querySelector("#player-score");
+const computerScoreDiv = document.querySelector("#computer-score");
+
+let pCard = null;
+let cCard = null;
+
+let playerChoice = "";
+let computerChoice = "";
+
+let playerScore = 0;
+let computerScore = 0;
+
+let i = 0;
+let typeSpeed = 40;
+let currentTimeout;
+// ---------------------------------Initialize---------------------------------
+
+
+
+// ---------------------------------Start---------------------------------
+typeText(footerText, "Your turn...");
+
+playerCards.forEach(card => {
+    card.addEventListener("click", e => {
+        pCard = card;
+        processPlayerChoice();
+    });
+});
+// ---------------------------------Start---------------------------------
+
+
+
+// ---------------------------------Main Logic---------------------------------
+function processPlayerChoice() {
+    playerChoice = getChoice(pCard).toLowerCase();
+    selectCard(pCard);    
+    turnOffInteractionCards();
+    processComputerChoice();
 }
 
 
-function playRound(playerChoice, computerChoice) {
+async function processComputerChoice() {
+    typeText(footerText, "Computer's turn...");
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    let randomIndex = Math.floor(Math.random() * 3);
+    let options = document.querySelector(".middle .right-section .options");
+    cCard = options.children[randomIndex].children[0];
+    selectCard(cCard);
+
+    computerChoice = getChoice(cCard).toLowerCase();
+
+    showResult();
+}
+
+
+function processResult(playerChoice, computerChoice) {
     let result = "";
 
     if (playerChoice === "rock" && computerChoice === "rock") result = "Draw";
@@ -27,40 +78,116 @@ function playRound(playerChoice, computerChoice) {
 }
 
 
-function getPlayerChoice() {
-    let action = prompt("Select your action!");
-    action = action.toLocaleLowerCase();
-    
-    if (action !== "rock" && action !== "paper" && action !== "scissor")
-        action = "invalid";
+function showResult() {
+    // Get the result
+    let result = processResult(playerChoice, computerChoice);
 
-    return action;
-}
-
-
-function getComputerChoice() {
-    let rnd = getRandomInteger(1, 4);
-    let choice = "";
-
-    switch (rnd) {
-        case 1:
-            choice = "rock";
-            break;
-        case 2:
-            choice = "paper";
-            break;
-        case 3:
-            choice = "scissor";
-            break;
-        default:
-            choice = "invalid";
+    // Increase score of either player or computer
+    if (result === "You win!") {
+        playerScore++;
+        playerScoreDiv.textContent = playerScore;
+    }
+    else if (result === "You lose.") {
+        computerScore++;
+        computerScoreDiv.textContent = computerScore;
     }
 
-    return choice;
+    // Whether to start next round, or finish the game
+    if (playerScore === 5) {
+        typeText(footerText, "You won the game!");
+        gameComplete();
+    }
+    else if (computerScore === 5) {
+        typeText(footerText, "Game Over");
+        gameComplete();
+    }
+    else {
+        typeText(footerText, result);
+        nextRound();
+    }
 }
 
 
-// The following page helped me get the solution - https://www.w3schools.com/js/js_random.asp
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+async function nextRound() {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    typeText(footerText, "Your turn...");
+
+    deselectCard(pCard);
+    deselectCard(cCard);
+
+    turnOnInteractionCards();
 }
+
+async function gameComplete() {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    typeText(footerText, "Your turn...");
+    
+    deselectCard(pCard);
+    deselectCard(cCard);
+
+    turnOnInteractionCards();
+
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDiv.textContent = playerScore;
+    computerScoreDiv.textContent = computerScore;
+}
+// ---------------------------------Main Logic---------------------------------
+
+
+
+// ---------------------------------Type Writing Effect---------------------------------
+// The following resource helped me - https://www.w3schools.com/howto/howto_js_typewriter.asp
+
+function typeText(element, text) {
+    i = 0;
+    element.textContent = "";
+    clearTimeout(currentTimeout);    
+    addChar(element, text);
+}
+
+function addChar(element, text) {
+    if (i < text.length) {
+      element.textContent += text.charAt(i);
+      i++;
+      
+      currentTimeout = setTimeout(addChar, typeSpeed, element, text);
+    }
+}
+// ---------------------------------Type Writing Effect---------------------------------
+
+
+
+// ---------------------------------Helper Methods---------------------------------
+function getChoice(card) {
+    for (let i = 0; i < card.childNodes.length; i++) {
+        if (card.childNodes[i].className === "desc") {
+            return card.childNodes[i].textContent;
+        }
+    }
+}
+
+
+function selectCard(card) {
+    card.classList.remove("card");
+    card.classList.add("card-selected");
+}
+
+function deselectCard(card) {
+    card.classList.remove("card-selected");
+    card.classList.add("card");
+}
+
+
+function turnOnInteractionCards() {
+    playerCards.forEach(card => {
+        card.style.pointerEvents = "auto";
+    });
+};
+
+function turnOffInteractionCards() {
+    playerCards.forEach(card => {
+        card.style.pointerEvents = "none";
+    });
+};
+// ---------------------------------Helper Methods---------------------------------
